59b659a9f21857482485f0c1d4a6c6850f16a503
scamps88 <scamps1@gmail.com>
2016-05-18 16:36:49 +0200

    [Example][task] Add body class - the body will contain the main interface, with days and weeks

---
+++ Example/Calendar/ASBodyV.swift
@@ -0,0 +1,17 @@
+//
+//  ASBodyV.swift
+//  Example
+//
+//  Created by alberto.scampini on 18/05/2016.
+//  Copyright © 2016 Alberto Scampini. All rights reserved.
+//
+
+import UIKit
+
+class ASBodyV: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
