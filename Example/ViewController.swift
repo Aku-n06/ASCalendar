2acadc7c743eb4ba97affb0cedeef907695889be
scamps88 <scamps1@gmail.com>
2016-05-18 15:06:32 +0200

    [Example][task] Add git

---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by alberto.scampini on 18/05/2016.
+//  Copyright © 2016 Alberto Scampini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
